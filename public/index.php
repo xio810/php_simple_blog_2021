5cdeb49ad1444050de7e817be923267a29a4bcef
xio810 <boi4806@naver.com>
2021-06-21 02:50:28 +0900

    index_test.php

--- public/index.php
+++ public/index.php
@@ -1,2 +1,23 @@
 <?php
-phpinfo();
+$article1 = [
+    "id" => "1",
+    "regDate" => "2021-06-12 03:32:52",
+    "title" => "제목1",
+    "body" => "내용1"
+];
+
+$article2 = [
+    "id" => "2",
+    "regDate" => "2021-06-12 03:32:52",
+    "title" => "제목2",
+    "body" => "내용2"
+];
+
+$articles = [];
+
+$articles[] = $article1
+$articles[] = $article2
+
+print_r($articles);
+exit;
+// echo -> 출력
---
+++ public/index_test.php
@@ -0,0 +1,15 @@
+<?php
+
+$article = [
+    "id" => "2",
+    "title" => "hello",
+    "regdate" => "2010-14-32",
+    "body" => "good night",
+];
+
+$article2 = [];
+
+
+print_r($article);
+print_r($article2);
+exit;
